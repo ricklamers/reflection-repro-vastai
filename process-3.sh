1cb6979e66ca33e202b6f849b03c9e353b700e36
Rick Lamers <ricklamers@gmail.com>
2024-09-23 14:25:07 -0700

    feat: add example lines for running IFEval with vanilla 3.1

--- process-3.sh
+++ process-3.sh
@@ -1,5 +1,7 @@
 cd simple-evals/ifeval
 python3 gen_results.py --model_name="glaiveai/Reflection-Llama-3.1-70B"
+# python3 gen_results.py --model_name="meta-llama/Meta-Llama-3.1-70B-Instruct" --no-reflection
+# python3 gen_results.py --model_name="meta-llama/Meta-Llama-3.1-70B-Instruct"
 python3 -m evaluation_main \
   --input_data=./data/ifeval_input_data.jsonl \
   --input_response_data=./data/reflection_output.jsonl \
