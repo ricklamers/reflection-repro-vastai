2b04cfd2774c8cfbbe35f3cc019739f3eb386c3a
Rick Lamers <ricklamers@gmail.com>
2024-09-23 14:23:40 -0700

    fix: run full evals and add llama 3.1 run examples

--- process-2.sh
+++ process-2.sh
@@ -1,5 +1,6 @@
 export OPENAI_API_KEY="{{OPENAI_API_KEY}}"
 echo $OPENAI_API_KEY
 cd simple-evals
-# python3 run_reflection_eval.py reflection_70b
-python3 run_reflection_eval.py reflection_70b --evals humaneval
+python3 run_reflection_eval.py reflection_70b
+# python3 run_reflection_eval.py llama_3.1_70b
+# python3 run_reflection_eval.py llama_3.1_70b_reflection_prompt
